9a6dce026868661e73a994b390a86b5fdde0cf39
Mendes Sousa <leoprofessionallogytech@outlook.com>
2023-02-17 19:44:51 -0300

    '@v1.0.20'

--- gitPush.cmd
+++ gitPush.cmd
@@ -1,3 +1,16 @@
 git add .
-git commit -m '@v1.0.19'
+git commit -m '@v1.0.20'
 git push
+
+
+@REM ## :busts_in_silhouette: Social Network
+
+@REM <div align="center">
+
+@REM [<img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white">](https://www.linkedin.com/in/leonardo-dos-santos-sousa-238651173/)
+
+@REM [<img src="https://img.shields.io/badge/github-%231877F2.svg?&style=for-the-badge&logo=github&logoColor=white&color=black">](https://github.com/LeonardoSousa89)
+
+@REM [<img src="https://img.shields.io/badge/email-%231877F2.svg?&style=for-the-badge&logo=email&logoColor=white&color=red">](mailto:leoprofessionallogytech@outlook.com)
+
+@REM </div>
